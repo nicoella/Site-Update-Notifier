d57ccd8e25574adec6bb7c5aeeebffca739a9918
Nicole Han <nicolehan04@gmail.com>
2023-07-10 22:51:30 -0400

    initialize frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Site Update Notifier</h1>
+      <p>
+        This is a site update tracking bot. Please enter the site you would like
+        to track updates on and a link to a webhook you would like the updates
+        to be posted to.
+      </p>
+      <form>
+        <label for="input1">Site to Track:</label>
+        <input type="text" id="input1" name="input1"></input>
+
+        <label for="input2">Webhook Link:</label>
+        <input type="text" id="input2" name="input2"></input>
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
