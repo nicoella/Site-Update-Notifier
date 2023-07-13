8adf436245e6f5b29f48909a4de1ab7cba75c593
Nicole Han <nicolehan04@gmail.com>
2023-07-12 20:06:41 -0400

    fix form, add status updates

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 import hashlib
-import json
+import json # tbd
+
+import asyncio
+import aiocron
 
 from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
@@ -21,16 +24,20 @@ collection = db["data"] # Update with your Collection name
 
 @app.route("/api/")
 def welcome():
-    return "api"
+    return "api is running"
 
 @app.route("/api/data", methods=["POST"])
 def insert_data():
-    site = request.form.get("site")
-    webhook = request.form.get("webhook")
+    print(request.json)
+    site = request.json.get("site")
+    webhook = request.json.get("webhook")
     hash_value = get_site_hash(site)
-    collection.insert({"site":site, "hash":hash_value, "webhook":webhook})
-    send_notification(webhook, "Webhook Added", "Your webhook has been added for the site "+site+". Update notifications will be sent here.")
-    return "test"
+    if not collection.insert_one({"site":site, "hash":hash_value, "webhook":webhook}).acknowledged:
+        return {"status": "bad insert"}
+    if not send_notification(webhook, "Webhook Added", "Your webhook has been added for the site "+site+". Update notifications will be sent here."):
+        print("error")
+        return {"status": "bad webhook"}
+    return {"status": "success"}
 
 @app.route("/api/ping")
 def ping_mongodb():
@@ -42,35 +49,50 @@ def ping_mongodb():
 
 def get_site_hash(url):
     response = requests.get(url)
-
     soup = BeautifulSoup(response.content, "html.parser")
-
     parsed_html_string = str(soup)
-    
     hash_object = hashlib.sha256(parsed_html_string.encode())
     hash_value = hash_object.hexdigest()
-    
     return hash_value
 
 
-def send_notification(webhook_url, title, description):
+def send_notification(webhook, title, description):
     data = {
-        "username" : "Site Update Notifier Webhook"
-    }
-    data["embeds"] = [
-        {
+        "username" : "Site Update Notifier Webhook",
+        "embeds": {
             "description" : description,
             "title" : title
         }
-    ]
-    result = requests.post(webhook_url, json = data)
+    }
     try:
+        result = requests.post(webhook, json = data)
         result.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        print(err)
-    else:
         print("Payload delivered successfully, code {}.".format(result.status_code))
+        return True
+    except Exception as err:
+        print(err)
+        return False
+        
+def check_update(url, hash, webhook):
+    cur_hash = get_site_hash(url)
+    if cur_hash != hash:
+        print("site updated!!!!")
+        send_notification(webhook, "Site Updated", "Your saved site " + url + " has had updates.")
+        
+async def get_all_data():
+    data = collection.find()
+    for site in data:
+        print(site['url']+" "+site['hash']+" "+site['webhook'])
+        check_update(site['url'], site['hash'], site['webhook'])
     
 # run backend    
 if __name__ == "__main__":
-    app.run(host="localhost", port=5000)
+    app.run(host="localhost", port=5000, debug=True)
+    
+# check updates
+#async def run_task():
+#    await aiocron.crontab("*/3 * * * *", func=get_all_data)
+
+#loop = asyncio.get_event_loop()
+
+#loop.run_until_complete(run_task())
