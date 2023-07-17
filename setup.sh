ad4530eea7b3ab983993f17ad73ce7fdc8655e47
Nicole Han <nicolehan04@gmail.com>
2023-07-16 21:51:35 -0400

    update setup.sh install

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ source venv/bin/activate
 # Install dependencies
 pip install beautifulsoup4 requests
 python -m pip install pymongo==3.11
+pip install dnspython
 
 # Install node modules
 cd frontend
